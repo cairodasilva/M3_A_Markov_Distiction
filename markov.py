c587c33fc41e3ecf365cc1636db41308eb46e07e
Cairo Dasilva <cdasilva@dhcp-200-12.student-wlan.bowdoin.edu>
2023-09-11 15:02:19 -0400

    Sept 11

--- markov.py
+++ markov.py
@@ -2,8 +2,7 @@
 
 
 import numpy as np
-
-
+from PIL import Image
 # We can add more of these later...keeping it simple for now!
 COLOR_CODES = {
 "COLOR_1": CODE,
@@ -21,57 +20,61 @@ COLOR_CODES = {
 "COLOR_13": CODE,
 "COLOR_14": CODE,
 "COLOR_15": CODE,
-
 }
+HORIZONTAL_PIXELS = 640;
+VERTICAL_PIXELS = 640;
+
 class MarkovBobRoss:
+
 def __init__(self, transition_matrix):
 
-self.transition_matrix = transition_matrix
-self.colors = list(transition_matrix.keys())
-def get_next_color(self, current_note):
+    self.transition_matrix = transition_matrix
+    self.colors = list(transition_matrix.keys())
 
-return np.random.choice(
-self.colors,
-p=[self.transition_matrix[current_color][next_color] \
-for next_note in self.colors]
+def get_next_color(self, current_color):
+
+    return np.random.choice(
+    self.colors,
+    p=[self.transition_matrix[current_color][next_color] \
+    for next_color in self.colors]
 )
-def compose_art(self, current_color="COLOR_1", art_length=3):
-"""Generates a sequence of notes.
-Args:
-current_note (str): the note of the song that we are currently
-looking at.
-song_length (int): how many notes we should generate for the
-song.
-"""
-artwork = []
-while len(artwork) < art_length:
-next_color = self.get_next_color(current_color)
-artwork.append(next_color)
-current_color = next_color
-return artwork
 
-def turn_to_2d(self):
-    """figure out how to turn one array into 2d array of hex codes """
-    return
+def compose_art(self, current_color="COLOR_1", art_length = HORIZONTAL_PIXELS*VERTICAL_PIXELS):
+    index = 0
+    artwork = np.zeros((HORIZONTAL_PIXELS, VERTICAL_PIXELS, 3), dtype=np.uint8)
+    while len(artwork) < art_length:
+        next_color = self.get_next_color(current_color)
+        artwork [HORIZONTAL_PIXELS/index, VERTICAL_PIXELS%index] = next_color
+        current_color = next_color
+        index += 1
+    return artwork
 
-def write_drawing(self):
-    """turns 2d array into painting"""
+
+
+def write_drawing(self, artwork):
+    """turns  array into painting"""
+    bobbyross = Image.fromarray(artwork, 'RGB')
+    bobbyross.save(bobbyross.png)
+    return bobbyross
 
 def main():
-Bob_Ross = MarkovBobRoss({
-"A": {"A": 0.3, "B": 0.4, "C": 0.3},
-"B": {"A": 0.7, "B": 0.2, "C": 0.1},
-"C": {"A": 0.1, "B": 0.7, "C": 0.2}
-"""make this actual"""
+    Bob_maker = MarkovBobRoss({
+        "A": {"A": 0.3, "B": 0.4, "C": 0.3},
+        "B": {"A": 0.7, "B": 0.2, "C": 0.1},
+        "C": {"A": 0.1, "B": 0.7, "C": 0.2}
+
 })
-new_song = song_maker.compose_melody(current_note="C", song_length=10)
-"""make the art with current color and length"""
 
-print("Writing song to file...") 
-"""print something cool here"""
+    new_art = Bob_maker.compose_art(current_color="COLOR_1", art_length=HORIZONTAL_PIXELS*VERTICAL_PIXELS)
+    """make the art with current color and length"""
 
-song_maker.drawing(new_song)
-print("Process completed!")
+    print("Writing song to file...") 
+    """print something cool here"""
+
+    img = Bob_maker.write_drawing(new_art)
+    img.show()
+    print("Process completed!")
+
 if __name__ == "__main__":
-main()
+    main()
 
