eaaf8e9536f18fb84d6ddf56781e3898b34f3608
Cairo Dasilva <cdasilva@dhcp-200-12.student-wlan.bowdoin.edu>
2023-09-13 23:13:12 -0400

    sept 13 lots to do

--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@
 import numpy as np
 from PIL import Image
 # We can add more of these later...keeping it simple for now!
-COLOR_CODES = {
+IMAGES = {
 "COLOR_1": [107, 174, 193],
 "COLOR_2": [30,65,69],
 "COLOR_3": [11,135,182],
@@ -13,6 +13,24 @@ COLOR_CODES = {
 "COLOR_8": [59,108,127],
 
 }
+POSITIONS = {
+    "POSITION_1": (0,0),
+    "POSITION_2": (0,0),
+    "POSITION_3": (0,0),
+    "POSITION_4": (0,0),
+    "POSITION_5": (0,0),
+    "POSITION_6": (0,0),
+    "POSITION_7": (0,0),
+    "POSITION_8": (0,0),
+    "POSITION_9": (0,0),
+    "POSITION_10": (0,0),
+    "POSITION_11": (0,0),
+    "POSITION_12": (0,0),
+    "POSITION_13": (0,0),
+    "POSITION_14": (0,0),
+    "POSITION_15": (0,0)
+}
+
 HORIZONTAL_PIXELS = 25;
 VERTICAL_PIXELS = 25;
 
@@ -21,27 +39,36 @@ class MarkovBobRoss:
     def __init__(self, transition_matrix):
 
      self.transition_matrix = transition_matrix
-     self.colors = list(transition_matrix.keys())
+     self.images = list(transition_matrix.keys())
 
-    def get_next_color(self, current_color):
+    def get_next_image(self, current_image):
 
         return np.random.choice(
-        self.colors,
-        p=[self.transition_matrix[current_color][next_color] \
-        for next_color in self.colors]
+        self.images,
+        p=[self.transition_matrix[current_image][next_image] \
+        for next_image in self.images]
     )
 
-    def compose_art(self, current_color="COLOR_1", art_length = HORIZONTAL_PIXELS*VERTICAL_PIXELS):
-        index = 1
-        artwork = np.zeros((HORIZONTAL_PIXELS, VERTICAL_PIXELS, 3), dtype=np.uint8)
-        for row in range(HORIZONTAL_PIXELS):
-            for col in range (VERTICAL_PIXELS):
+    def get_next_position(self, current_position):
+        return np.random.choice(list(self.transition_matrix_positions.keys()),
+            p=list(self.transition_matrix_positions[current_position].values())
+        )
+      
+        
+    
 
-                next_color = self.get_next_color(current_color)
-                artwork [row, col] = COLOR_CODES[next_color]
-                current_color = next_color
-                index += 1
-        return artwork
+
+    def compose_collage(self, current_image="COLOR_1", num_images = 15, current_position = (0,0)):
+        collage = Image.new("RGB", (HORIZONTAL_PIXELS, VERTICAL_PIXELS))
+        for row in range(num_images):
+            next_image = self.get_next_image(current_image)
+            next_position = self.get_next_position(current_position)
+            image = Image.open(next_image)
+            collage.paste(image, next_position)
+
+
+                
+        return collage
 
 
 
@@ -50,6 +77,32 @@ class MarkovBobRoss:
         bobbyross = Image.fromarray(artwork, 'RGB')
         bobbyross.save('bobbyross.png')
         return bobbyross
+
+    transition_matrix_positions = {}
+    def make_postition_t_matrix(self, current_position):
+        next_positions = []
+        movements = [
+        (-1, 0, 0.1),  # up
+        (1, 0, 0.1),   # down
+        (0, -1, 0.1),  #left
+        (0, 1, 0.2), # right
+        (-2, 0, 0.1), #up option 2
+        (-2, 0, 0.2), # down option 2
+        (0, -2, 0.1), # left option 2
+        (0, 2, 0.1) # right option 2
+        ]
+        for x_transition, y_transition, probability in movements:
+            x , y = current_position
+            newx = x+x_transition
+            newy = y+y_transition
+            if 0 <= newx < HORIZONTAL_PIXELS and 0 <= newy < VERTICAL_PIXELS:
+             next_positions[(newx, newy)] = probability
+            
+
+        #here you need to make sure the probabilites add up to 1
+        #and then if not you need to get the difference and scale it to all of the probs at the moment
+        #then return next_prob array
+        # to make the transitions matrix you need to go to all of the pixels with nested loops and then set the array to what the function would say
 
 def main():
         Bob_maker = MarkovBobRoss({
